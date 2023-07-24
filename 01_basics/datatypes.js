f3fb172cca33eb966725fbbee487ada8dc5836ad
bukkir <140377440+bukkir@users.noreply.github.com>
2023-07-24 17:02:25 +0000

    datatpes

---
+++ 01_basics/datatypes.js
@@ -0,0 +1,8 @@
+console.log(typeof "ravi");
+console.log(typeof true);
+
+let a=25
+console.log(typeof(a));
+
+console.log(typeof null);
+console.log(typeof undefined);
---
+++ 01_basics/variables.js
@@ -0,0 +1,12 @@
+const accountId = 4578
+let accountEmail = "ravi@gmail.com"
+var accountPwd = "1234"
+
+// accountId =855
+accountCity = "Adoni"
+accountEmail = "test.com@g.com"
+accountPwd = "478954"
+console.log(accountCity);
+console.log(accountId);
+
+console.table([accountEmail,accountId,accountPwd])
